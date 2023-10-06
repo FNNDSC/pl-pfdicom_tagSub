21f22d149a5c1f424f7bb9a19b64a00932984e37
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-10-06 11:24:54 -0400

    memory and version bump

--- dcm_tagSub/dcm_tagSub.py
+++ dcm_tagSub/dcm_tagSub.py
@@ -228,7 +228,7 @@ class Dcm_tagSub(ChrisApp):
     MIN_NUMBER_OF_WORKERS   = 1    # Override with the minimum number of workers as int
     MAX_NUMBER_OF_WORKERS   = 1    # Override with the maximum number of workers as int
     MIN_CPU_LIMIT           = 2000 # Override with millicore value as int (1000 millicores == 1 CPU core)
-    MIN_MEMORY_LIMIT        = 2000  # Override with memory MegaByte (MB) limit as int
+    MIN_MEMORY_LIMIT        = 32000  # Override with memory MegaByte (MB) limit as int
     MIN_GPU_LIMIT           = 0    # Override with the minimum number of GPUs as int
     MAX_GPU_LIMIT           = 0    # Override with the maximum number of GPUs as int
 
